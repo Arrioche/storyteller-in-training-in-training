3f9ce93b11239fba8a40f089ec7f206fcc56d98b
Arrioche <kittydurmont@gmail.com>
2020-01-14 13:22:25 -0800

    I made it detect uwu and uwu variants even when they are seperated by spaces

--- Storyteller in Training in Training.py
+++ Storyteller in Training in Training.py
@@ -1,8 +1,8 @@
-import discord#https://discordpy.readthedocs.io/en/latest/index.html
+import discord  # https://discordpy.readthedocs.io/en/latest/index.html
 import logging
 
 logging.basicConfig(level=logging.INFO)
-client = discord.Client()#c/p from above link, idk what it does
+client = discord.Client()  # c/p from above link, idk what it does
 
 state = 'added'
 channels = []
@@ -12,26 +12,26 @@ stupid = None
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))#when this pops up in terminal you can use the bot
+    print('We have logged in as {0.user}'.format(client))  # when this pops up in terminal you can use the bot
 
 @client.event
-async def on_message(message):#looks at every message sent in the server
+async def on_message(message):  # looks at every message sent in the server
     global state
     global channels
     global day
     global setupchannel
-    global stupid#asynchronous functions are hard
+    global stupid  # asynchronous functions are hard
 
     msg = message.content
     msgch = message.channel
 
-    if message.author == client.user:#so it doesn't look at its own messages
+    if message.author == client.user:  # so it doesn't look at its own messages
         return
 
     if msg.startswith('!setup') and state != 'channel':
         if not setupchannel:
-            setupchannel = msgch#sets one channel to be used for all admin commands
-            await msgch.send('Setup channel has been set to `' + setupchannel.name + '`. This is permanent.')#if you're new to the library [channel].send('txt') sends txt in [channel]
+            setupchannel = msgch  # sets one channel to be used for all admin commands
+            await msgch.send('Setup channel has been set to `' + setupchannel.name + '`. This is permanent.')  # if you're new to the library [channel].send('txt') sends txt in [channel]
         if setupchannel == msgch:
             state = 'channel'
             await msgch.send('Please set channels with `!add`, `!remove`, `!catadd`, `!catrm`, `!servadd`, or `!servrm`. Type `!done` when finished')
@@ -42,14 +42,14 @@ async def on_message(message):#looks at every message sent in the server
             if msgch in channels:
                 await msgch.send('This channel has already been added')
             else:
-                channels.append(msgch)#adds a specific channel to the list to be divided.
+                channels.append(msgch)  # adds a specific channel to the list to be divided.
                 await msgch.send('Added channel `' + msgch.name + '`')
         if msg.startswith('!catadd'):
-                if msgch.category:#if the channel belongs to a category
+                if msgch.category:  # if the channel belongs to a category
                     cats = msgch.category.text_channels
                     for ch in cats:
                         if ch not in channels:
-                            channels.append(ch)#adds each channel that isn't already on the list
+                            channels.append(ch)  # adds each channel that isn't already on the list
                     await msgch.send('All channels in category `' + msgch.category.name + '` have been added.')
                 else:
                     await msgch.send('This channel does not appear to be part of a category; please use `!add`')
@@ -62,7 +62,7 @@ async def on_message(message):#looks at every message sent in the server
             if msgch not in channels:
                 await msgch.send('This channel has not been added')
                 return
-            channels.remove(msgch)#removes the channel from the list
+            channels.remove(msgch)  # removes the channel from the list
             await msgch.send('Removed channel `' + msgch.name + '`')
         if msg.startswith('!catrm'):
             if msgch.category:
@@ -94,16 +94,16 @@ async def on_message(message):#looks at every message sent in the server
             if day == 0:
                 div = ('————————— NEW GAME —————————')
             else:
-                div = ('————————— DAY ' + str(day) + ' —————————')#i really need to figure out how to do this more cleanly
-            for chan in channels:#where the magic happens
-                await chan.send(div)#puts the day divider in every channel slated to be divided
+                div = ('————————— DAY ' + str(day) + ' —————————')  # i really need to figure out how to do this more cleanly
+            for chan in channels:  # where the magic happens
+                await chan.send(div)  # puts the day divider in every channel slated to be divided
             day += 1
             if stupid:
                 for player in message.guild.members:
                     if stupid in player.roles:
                         updated = player.roles
                         updated.remove(stupid)
-                        await player.edit(roles = updated, reason='A new day dawns, and all is forgiven')#Note: currently does not appear to work. also does not appear to affect function.
+                        await player.edit(roles = updated, reason='A new day dawns, and all is forgiven')  # Note: currently does not appear to work. also does not appear to affect function.
         if msg.startswith('!newgame'):
             if state != 'dividing':
                 await msgch.send('Please finish setup via either `!setup` or `!done`.')
@@ -115,33 +115,34 @@ async def on_message(message):#looks at every message sent in the server
                 await msgch.send('Please finish setup via either `!setup` or `!done`.')
                 return
             digits = ['0','1','2','3','4','5','6','7','8','9']
-            if msg[-1] not in digits:#i'm sure there's a better way to do this like with numpy or smth
+            if msg[-1] not in digits:  # i'm sure there's a better way to do this like with numpy or smth
                 await msgch.send('Please enter a valid digit')
                 return
             day = int(msg[-1])
             await msgch.send('The next day to be sent will be Day ' + str(day) + '. Please use `!divide` to send day dividers')
         if msg.startswith('!ping'):
-            #chanOut = ''
-            #first = 0
-            #if channels:
+            # chanOut = ''
+            # first = 0
+            # if channels:
             #    for ch in channels:
             #        if first:
             #            chanOut += ', '
             #        graved = '`' + ch.name + '`'
             #        chanOut += graved
             #        first = None
-            #else:
+            # else:
             #    chanOut = '`None`'
-            await msgch.send('Ping received.')#no longer sends list of all channels - went over max message limit
-            if state == 'added':#sends a status update
+            await msgch.send('Ping received.')  # no longer sends list of all channels - went over max message limit
+            if state == 'added':  # sends a status update
                 await msgch.send('Please setup via `!setup`.')
             elif state == 'channel':
                 await msgch.send('Please finish setup, then type `!done`.')
             elif state == 'dividing':
                 await msgch.send('Please send day dividers via `divide`.')
-    banned = ['uwu','Uwu','UWu','UwU','uWu','uWU','uwU','UWU']
+    banned = ['uwu', 'uωu', 'uшu']
+    cleanMsg = msg.lower().replace(" ","")
     for word in banned:
-        if word in msg:
+        if word in cleanMsg:
             await msgch.send('***NO UWU***')
             if stupid:
                 sinner = message.author
